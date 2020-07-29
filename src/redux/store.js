e43f8e99f5f47cd01e52f6896673bb23c3a5dc5f
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2020-07-29 14:10:31 +0530

    redux error fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,9 @@ const store = createStore(
 	intialState,
 	compose(
 		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		window.__REDUX_DEVTOOLS_EXTENSION__
+			? window.__REDUX_DEVTOOLS_EXTENSION__()
+			: (f) => f
 	)
 );
 
